3734f3cda8f75510749c1ca7e0f9eca54cf16231
lukexyz <lukedwoods@gmail.com>
2019-05-29 17:45:39 +0100

    Fix dict enumerator

--- display_test.py
+++ display_test.py
@@ -6,29 +6,24 @@ cap = cv2.VideoCapture('data/JC_C_Reshoot_06_single_1080x1620.mp4')
 vd = OrderedDict()
 i = 0
 
+print('loading mp4')
 while(cap.isOpened()):
     # Capture frame-by-frame
     ret, frame = cap.read()
     if ret is True:
         vd[i] = frame
-        cv2.imshow('Frame', frame)
-        key = cv2.waitKey(100)
     else:
         break
     i += + 1
 print(len(vd), 'frames loaded')
 
 
-# for img in vd:
+for i in range(200):
 
-#     # Image Resize for dev
-#     # outframe = cv.resize(img, None, fx=0.4, fy=0.4)
+    # Image Resize for dev
+    # outframe = cv.resize(img, None, fx=0.4, fy=0.4)
 
-#     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = vd[i]
+    cv2.imshow('frame', img)
+    key = cv2.waitKey(20)
 
-#     cv2.imshow('frame', img)
-
-#     key = cv2.waitKey(100)  # pause for 100 ms
-#     if key == 27:  # if ESC is pressed, exit loop
-#         cv2.destroyAllWindows()
-#         break
